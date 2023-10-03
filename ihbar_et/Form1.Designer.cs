a6bbee3f92835cd8c8d51e79877b23c894220f10
Kemal Alper <kmlkckk0@gmail.com>
2023-10-03 17:00:47 +0300

    Deneme GUI

---
+++ ihbar_et/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ihbar_et
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ilComboBox = new System.Windows.Forms.ComboBox();
+            this.ilceComboBox = new System.Windows.Forms.ComboBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblTown = new System.Windows.Forms.Label();
+            this.adresRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.lblAddressInfo = new System.Windows.Forms.Label();
+            this.ihbarEt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ilComboBox
+            // 
+            this.ilComboBox.FormattingEnabled = true;
+            this.ilComboBox.Items.AddRange(new object[] {
+            "a",
+            "a",
+            "a",
+            "a",
+            "a",
+            "aa",
+            "a",
+            "a",
+            "aa",
+            "",
+            "b",
+            "b",
+            "b",
+            "b",
+            "b",
+            "b",
+            "bb",
+            "b",
+            "",
+            "b",
+            "b",
+            "b",
+            "b",
+            "b",
+            "c",
+            "",
+            "c",
+            "c",
+            "c",
+            "cc",
+            "",
+            "cc",
+            "c",
+            "c",
+            "c",
+            "c",
+            "c",
+            "",
+            "cc",
+            "c"});
+            this.ilComboBox.Location = new System.Drawing.Point(120, 100);
+            this.ilComboBox.Name = "ilComboBox";
+            this.ilComboBox.Size = new System.Drawing.Size(121, 21);
+            this.ilComboBox.TabIndex = 0;
+            this.ilComboBox.SelectedIndexChanged += new System.EventHandler(this.ilComboBox_SelectedIndexChanged);
+            // 
+            // ilceComboBox
+            // 
+            this.ilceComboBox.FormattingEnabled = true;
+            this.ilceComboBox.Location = new System.Drawing.Point(120, 140);
+            this.ilceComboBox.Name = "ilceComboBox";
+            this.ilceComboBox.Size = new System.Drawing.Size(121, 21);
+            this.ilceComboBox.TabIndex = 1;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(38, 103);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(31, 13);
+            this.lblCity.TabIndex = 2;
+            this.lblCity.Text = "Şehir";
+            // 
+            // lblTown
+            // 
+            this.lblTown.AutoSize = true;
+            this.lblTown.Location = new System.Drawing.Point(38, 143);
+            this.lblTown.Name = "lblTown";
+            this.lblTown.Size = new System.Drawing.Size(24, 13);
+            this.lblTown.TabIndex = 3;
+            this.lblTown.Text = "İlçe";
+            // 
+            // adresRichTextBox
+            // 
+            this.adresRichTextBox.Location = new System.Drawing.Point(120, 178);
+            this.adresRichTextBox.Name = "adresRichTextBox";
+            this.adresRichTextBox.Size = new System.Drawing.Size(299, 46);
+            this.adresRichTextBox.TabIndex = 4;
+            this.adresRichTextBox.Text = "";
+            // 
+            // lblAddressInfo
+            // 
+            this.lblAddressInfo.AutoSize = true;
+            this.lblAddressInfo.Location = new System.Drawing.Point(38, 181);
+            this.lblAddressInfo.Name = "lblAddressInfo";
+            this.lblAddressInfo.Size = new System.Drawing.Size(58, 13);
+            this.lblAddressInfo.TabIndex = 5;
+            this.lblAddressInfo.Text = "Açık Adres";
+            // 
+            // ihbarEt
+            // 
+            this.ihbarEt.Location = new System.Drawing.Point(137, 259);
+            this.ihbarEt.Name = "ihbarEt";
+            this.ihbarEt.Size = new System.Drawing.Size(189, 113);
+            this.ihbarEt.TabIndex = 6;
+            this.ihbarEt.Text = "İhbar Et !";
+            this.ihbarEt.UseVisualStyleBackColor = true;
+            this.ihbarEt.Click += new System.EventHandler(this.ihbarEtButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 571);
+            this.Controls.Add(this.ihbarEt);
+            this.Controls.Add(this.lblAddressInfo);
+            this.Controls.Add(this.adresRichTextBox);
+            this.Controls.Add(this.lblTown);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.ilceComboBox);
+            this.Controls.Add(this.ilComboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ilComboBox;
+        private System.Windows.Forms.ComboBox ilceComboBox;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblTown;
+        private System.Windows.Forms.Label lblAddressInfo;
+        private System.Windows.Forms.Button ihbarEt;
+        private System.Windows.Forms.RichTextBox adresRichTextBox;
+    }
+}
+
---
+++ ihbar_et/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ihbar_et
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Form yüklenirken il ComboBox'ına örnek il verileri ekleyelim
+            ilComboBox.Items.Add("İstanbul");
+            ilComboBox.Items.Add("Ankara");
+            ilComboBox.Items.Add("İzmir");
+            // İlçe ComboBox'ını boşaltalım
+            ilceComboBox.Items.Clear();
+        }
+
+        private void ilComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // İlçe ComboBox'ını temizleyin
+            ilceComboBox.Items.Clear();
+
+            // Seçilen il'e göre ilçeleri ekleyin (örnek veri)
+            string secilenIl = ilComboBox.SelectedItem.ToString();
+            if (secilenIl == "İstanbul")
+            {
+                ilceComboBox.Items.Add("Kadıköy");
+                ilceComboBox.Items.Add("Beşiktaş");
+                ilceComboBox.Items.Add("Üsküdar");
+                // Diğer ilçeleri eklemeye devam edin
+            }
+            else if (secilenIl == "Ankara")
+            {
+                ilceComboBox.Items.Add("Çankaya");
+                ilceComboBox.Items.Add("Kızılay");
+                ilceComboBox.Items.Add("Yenimahalle");
+                // Diğer ilçeleri eklemeye devam edin
+            }
+            // Diğer iller için aynı işlemi yapın
+        }
+
+        private void ihbarEtButton_Click(object sender, EventArgs e)
+        {
+            // İl, ilçe ve açık adres bilgilerini alın
+            string secilenIl = ilComboBox.SelectedItem?.ToString();
+            string secilenIlce = ilceComboBox.SelectedItem?.ToString();
+            string acikAdres = adresRichTextBox.Text;
+
+            // Gerekli alanların dolu olup olmadığını kontrol edin
+            if (string.IsNullOrWhiteSpace(secilenIl) || string.IsNullOrWhiteSpace(secilenIlce) || string.IsNullOrWhiteSpace(acikAdres))
+            {
+                MessageBox.Show("Lütfen il, ilçe ve açık adres alanlarını doldurun.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // İşlemi burada sonlandırın
+            }
+
+            // İhbarı iletmek için gerekli işlemleri burada gerçekleştirin
+            // Örneğin, bu bilgileri bir veritabanına kaydedebilir veya bir sunucuya iletebilirsiniz
+
+            // İhbarın başarıyla iletilip iletilmediğini kontrol edin
+            if (IhbariIletildiMi(secilenIl, secilenIlce, acikAdres))
+            {
+                MessageBox.Show("İhbar başarıyla iletilmiştir.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("İhbar iletimi başarısız oldu. Lütfen tekrar deneyin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // İhbarı iletmek için gereken işlemi gerçekleştiren örnek bir fonksiyon
+        private bool IhbariIletildiMi(string il, string ilce, string acikAdres)
+        {
+            // Burada ihbarın iletilip iletilmediğini kontrol etmek için gereken kodu ekleyin
+            // Örneğin, bir veritabanına kaydetmek veya bir sunucuya göndermek gibi işlemler yapabilirsiniz
+
+            // Örnek olarak her zaman başarılı olduğunu varsayalım
+            return true;
+        }
+
+    }
+}
